49c3b0bf90d4dfdc845882491421f9ef555512d1
JUNEBOK <leebok0812@gmail.com>
2021-03-10 22:28:25 +0900

    CharacterTest

--- Chapter1/src/ch07/DoubleTest.java
+++ Chapter1/src/ch07/DoubleTest.java
@@ -3,8 +3,11 @@ package ch07;
 public class DoubleTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		double dNum = 3.14;
+		float fNum = 3.14f;
+		
+		System.out.println(dNum);
+		System.out.println(fNum);
 	}
 
 }
