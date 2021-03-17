e9f817b132d7dacaa3c5a7dd15e42ad6413b0d2b
JUNEBOK <leebok0812@gmail.com>
2021-03-17 22:24:40 +0900

    IfElseTest

--- Chapter1/src/ch13/BitOperationTest.java
+++ Chapter1/src/ch13/BitOperationTest.java
@@ -12,7 +12,7 @@ public class BitOperationTest {
 		System.out.println(num1 ^ num2);
 		System.out.println(~num1);
 		
-		System.out.println(num1 << 2);
+		System.out.println(num1 << 2); // 값까지 바꾸려면 <<= '='대입연산자까지 써야함.
 		System.out.println(num1 );
 		
 		
