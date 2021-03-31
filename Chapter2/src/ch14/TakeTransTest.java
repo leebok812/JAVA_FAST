38af94f2033960ac354e6146ca97bc8abf92bb98
JUNEBOK <leebok0812@gmail.com>
2021-03-31 22:12:46 +0900

    Bus+Student+Subway+TakeTransTest

---
+++ Chapter2/src/ch14/TakeTransTest.java
@@ -0,0 +1,27 @@
+package ch14;
+
+public class TakeTransTest {
+
+	public static void main(String[] args) {
+
+		Student studentJ = new Student("JAMES",5000);
+		Student studentT = new Student("TOMAS",10000);
+		
+		Bus bus100 = new Bus(100);
+		Bus bus500 = new Bus(500);
+		
+		studentJ.takeBus(bus100);
+		
+		Subway greenSubway = new Subway(2);
+		studentT.takeSubway(greenSubway);
+		
+		studentJ.showInfo();
+		studentT.showInfo();
+		System.out.println("==================");
+		bus100.showBusInfo();
+		greenSubway.showBusInfo();
+		
+		
+	}
+
+}
