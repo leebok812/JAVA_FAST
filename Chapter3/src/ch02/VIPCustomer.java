817fb477c8300f007c3523fddf28b5bbb536bd80
JUNEBOK <leebok0812@gmail.com>
2021-04-29 22:21:06 +0900

    Customer+CustomerTest+VIPCustomer

---
+++ Chapter3/src/ch02/VIPCustomer.java
@@ -0,0 +1,31 @@
+package ch02;
+
+public class VIPCustomer extends Customer {
+
+	/*
+	 * private int customerID; private String customerName; private String
+	 * customerGrade; int bonusPoint; double bonusRatio;
+	 */    // Customer에서 상속 받는 것들
+	
+	double salesRatio;
+	private String agentID;
+	
+	public VIPCustomer() {
+		
+		bonusRatio = 0.05;
+		salesRatio = 0.1;
+		customerGrade = "VIP";
+	}
+
+	public String getAgentID() {
+		return agentID;
+	}
+
+	public void setAgentID(String agentID) {
+		this.agentID = agentID;
+	}
+	
+	
+	
+	
+}
