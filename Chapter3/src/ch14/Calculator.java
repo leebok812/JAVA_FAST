343241ef902965e6978b92981f22fea7aa022cbb
JUNEBOK <leebok0812@gmail.com>
2021-05-19 12:19:38 +0900

    Calc+Calculator+CalculatorTest+CompleteCalc

---
+++ Chapter3/src/ch14/Calculator.java
@@ -0,0 +1,15 @@
+package ch14;
+
+public abstract class Calculator implements Calc { // 모두다 구현하지 않기때문에 abstract 함
+
+	@Override
+	public int add(int num1, int num2) {
+		return num1 + num2;
+	}
+
+	@Override
+	public int substract(int num1, int num2) {
+		return num1 - num2;
+	}
+
+}
