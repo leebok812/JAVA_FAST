eee5d0c774b0dbe65434b34099d6ff3c7487489c
JUNEBOK <leebok0812@gmail.com>
2021-07-14 21:16:50 +0900

    예외처리하기와 미루기(1)

---
+++ Chapter6/src/ch09/AutoCloseTest.java
@@ -0,0 +1,17 @@
+package ch09;
+
+public class AutoCloseTest {
+
+	public static void main(String[] args) {
+
+		AutoCloseableObj obj = new AutoCloseableObj();
+
+		try (obj) {
+			throw new Exception(); // Exception 강제 발생
+		} catch (Exception e) {
+			System.out.println("exception");
+		}
+		System.out.println("end");
+	}
+
+}
